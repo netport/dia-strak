9f3c90f13edaff109124b18f5b40d64c0d26dc9e
Andrey <git@andrey.se>
2016-03-07 23:21:44 +0100

    Application shell & some UI

---
+++ Prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by Andrey on 07/03/16.
+//  Copyright © 2016 Tap Away. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
